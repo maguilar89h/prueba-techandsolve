76ee6d1db35e810353756746321bf3d7f4167406
Manuel Aguilar <maguilar89h@gmail.com>
2018-05-07 13:55:54 -0500

    commit inicial

---
+++ vuelo/src/main/java/com/prueba/vuelo/VueloApplication.java
@@ -0,0 +1,12 @@
+package com.prueba.vuelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueloApplication.class, args);
+	}
+}
